ae38d303d7b0b125561599414b470b1a0e6e7f80
shkryl <ShkrylAndrei@rambler.ru>
2025-09-11 13:21:15 +0400

    Made examples SpringApplicationEvents.

---
+++ src/main/java/info/shkryl/SpringApplicationEvents/TicketBookedEvent.java
@@ -0,0 +1,20 @@
+package info.shkryl.SpringApplicationEvents;
+
+import java.time.LocalDateTime;
+
+public class TicketBookedEvent {
+    private final String flightNumber;
+    private final String passengerEmail;
+    private final LocalDateTime bookingTime;
+
+    public TicketBookedEvent(String flightNumber, String passengerEmail) {
+        this.flightNumber = flightNumber;
+        this.passengerEmail = passengerEmail;
+        this.bookingTime = LocalDateTime.now();
+    }
+
+    // Геттеры
+    public String getFlightNumber() { return flightNumber; }
+    public String getPassengerEmail() { return passengerEmail; }
+    public LocalDateTime getBookingTime() { return bookingTime; }
+}
